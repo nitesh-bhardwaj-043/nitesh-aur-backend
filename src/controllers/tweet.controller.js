2c8d5c4da5b4dd13cd1f18053ef4f679fdce8c01
Nitesh Bhardwaj <nitesh123bhardwaj123@gmail.com>
2024-03-28 21:10:25 +0530

    fixed avatar.url in tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -34,7 +34,7 @@ const getUserTweets = asyncHandler(async (req, res) => {
           {
             $project: {
               username: 1,
-              avatar: 1,
+              "avatar.url": 1,
             },
           },
         ],
